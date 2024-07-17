6c87bfd4a1fcc18d41b225de55275d051c5f349e
Gagasbandas <kjjcds@gmail.com>
2024-07-17 18:01:54 +0900

    Architecture of f0 Manager

--- gui/f0.py
+++ gui/f0.py
@@ -63,8 +63,8 @@ class f0Manager:
             audio,
             sampling_rate,
             hop_length,
-            f0_min,
-            f0_max,
+            self.f0_min,
+            self.f0_max,
             model,
             batch_size=hop_length * 2,
             device=device,
@@ -73,7 +73,7 @@ class f0Manager:
 
         f0 = pitch.squeeze(0).cpu().float().numpy()
         p_len = p_len or wav_numpy.shape[0] // hop_length
-        f0 = _resize_f0(f0, p_len)
+        f0 = self._resize_f0(f0, p_len)
         return f0
 
 
@@ -106,94 +106,57 @@ class f0Manager:
         res = np.nan_to_num(target)
         return res
 
-def normalize_f0(
-    f0: FloatTensor, x_mask: FloatTensor, uv: FloatTensor, random_scale=True
-) -> FloatTensor:
-    # calculate means based on x_mask
-    uv_sum = torch.sum(uv, dim=1, keepdim=True)
-    uv_sum[uv_sum == 0] = 9999
-    means = torch.sum(f0[:, 0, :] * uv, dim=1, keepdim=True) / uv_sum
+    def normalize_f0(
+        self, f0: FloatTensor, x_mask: FloatTensor, uv: FloatTensor, random_scale=True
+    ) -> FloatTensor:
+        # calculate means based on x_mask
+        uv_sum = torch.sum(uv, dim=1, keepdim=True)
+        uv_sum[uv_sum == 0] = 9999
+        means = torch.sum(f0[:, 0, :] * uv, dim=1, keepdim=True) / uv_sum
 
-    if random_scale:
-        factor = torch.Tensor(f0.shape[0], 1).uniform_(0.8, 1.2).to(f0.device)
-    else:
-        factor = torch.ones(f0.shape[0], 1).to(f0.device)
-    # normalize f0 based on means and factor
-    f0_norm = (f0 - means.unsqueeze(-1)) * factor.unsqueeze(-1)
-    if torch.isnan(f0_norm).any():
-        exit(0)
-    return f0_norm * x_mask
-
-
-def interpolate_f0(
-    f0: ndarray[Any, dtype[float32]]
-) -> tuple[ndarray[Any, dtype[float32]], ndarray[Any, dtype[float32]]]:
-    data = np.reshape(f0, (f0.size, 1))
-
-    vuv_vector = np.zeros((data.size, 1), dtype=np.float32)
-    vuv_vector[data > 0.0] = 1.0
-    vuv_vector[data <= 0.0] = 0.0
-
-    ip_data = data
-
-    frame_number = data.size
-    last_value = 0.0
-    for i in range(frame_number):
-        if data[i] <= 0.0:
-            j = i + 1
-            for j in range(i + 1, frame_number):
-                if data[j] > 0.0:
-                    break
-            if j < frame_number - 1:
-                if last_value > 0.0:
-                    step = (data[j] - data[i - 1]) / float(j - i)
-                    for k in range(i, j):
-                        ip_data[k] = data[i - 1] + step * (k - i + 1)
-                else:
-                    for k in range(i, j):
-                        ip_data[k] = data[j]
-            else:
-                for k in range(i, frame_number):
-                    ip_data[k] = last_value
+        if random_scale:
+            factor = torch.Tensor(f0.shape[0], 1).uniform_(0.8, 1.2).to(f0.device)
         else:
-            ip_data[i] = data[i]
-            last_value = data[i]
-
-    return ip_data[:, 0], vuv_vector[:, 0]
-
-def _resize_f0(
-    x: ndarray[Any, dtype[float32]], target_len: int
-) -> ndarray[Any, dtype[float32]]:
-    source = np.array(x)
-    source[source < 0.001] = np.nan
-    target = np.interp(
-        np.arange(0, len(source) * target_len, len(source)) / target_len,
-        np.arange(0, len(source)),
-        source,
-    )
-    res = np.nan_to_num(target)
-    return res
+            factor = torch.ones(f0.shape[0], 1).to(f0.device)
+        # normalize f0 based on means and factor
+        f0_norm = (f0 - means.unsqueeze(-1)) * factor.unsqueeze(-1)
+        if torch.isnan(f0_norm).any():
+            exit(0)
+        return f0_norm * x_mask
 
 
-def f0_to_coarse(f0: torch.Tensor | float):
-    is_torch = isinstance(f0, torch.Tensor)
-    f0_mel = 1127 * (1 + f0 / 700).log() if is_torch else 1127 * np.log(1 + f0 / 700)
-    f0_mel[f0_mel > 0] = (f0_mel[f0_mel > 0] - f0_mel_min) * (f0_bin - 2) / (
-        f0_mel_max - f0_mel_min
-    ) + 1
+    def interpolate_f0(
+        f0: ndarray[Any, dtype[float32]]
+    ) -> tuple[ndarray[Any, dtype[float32]], ndarray[Any, dtype[float32]]]:
+        data = np.reshape(f0, (f0.size, 1))
 
-    f0_mel[f0_mel <= 1] = 1
-    f0_mel[f0_mel > f0_bin - 1] = f0_bin - 1
-    f0_coarse = (f0_mel + 0.5).long() if is_torch else np.rint(f0_mel).astype(np.int)
-    assert f0_coarse.max() <= 255 and f0_coarse.min() >= 1, (
-        f0_coarse.max(),
-        f0_coarse.min(),
-    )
-    return f0_coarse
+        vuv_vector = np.zeros((data.size, 1), dtype=np.float32)
+        vuv_vector[data > 0.0] = 1.0
+        vuv_vector[data <= 0.0] = 0.0
 
+        ip_data = data
 
-f0_bin = 256
-f0_max = 1100.0
-f0_min = 50.0
-f0_mel_min = 1127 * np.log(1 + f0_min / 700)
-f0_mel_max = 1127 * np.log(1 + f0_max / 700)
+        frame_number = data.size
+        last_value = 0.0
+        for i in range(frame_number):
+            if data[i] <= 0.0:
+                j = i + 1
+                for j in range(i + 1, frame_number):
+                    if data[j] > 0.0:
+                        break
+                if j < frame_number - 1:
+                    if last_value > 0.0:
+                        step = (data[j] - data[i - 1]) / float(j - i)
+                        for k in range(i, j):
+                            ip_data[k] = data[i - 1] + step * (k - i + 1)
+                    else:
+                        for k in range(i, j):
+                            ip_data[k] = data[j]
+                else:
+                    for k in range(i, frame_number):
+                        ip_data[k] = last_value
+            else:
+                ip_data[i] = data[i]
+                last_value = data[i]
+
+        return ip_data[:, 0], vuv_vector[:, 0]
